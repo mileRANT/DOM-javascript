dca338ee81383cc529dce54119b176b3cfaf4902
mileRANT <liem.htran@outlook.com>
2023-05-01 14:03:53 -0400

    Practice with DOM and javascript

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 alert("This is within external index.js file. Hello")
 
 document.querySelector("h1").innerHTML = "Good Bye";
+
+//document.getElementsByTagName("li")       //gives an array
+document.getElementsByTagName("li")[2].style.color = "purple";
+
+// document.getElementsByClassName("btn")[0].style.color = "green";
+
+document.getElementsByTagName("input")[0].click();
+
+document.getElementById("title").innerHTML = "Hello Again!";
+
+document.querySelector("#title").innerHTML = "And then bye again.";
+
+document.querySelector("li a").innerHTML = "Google google google";
