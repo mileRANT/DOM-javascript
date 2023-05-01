7ebf1b67f8188210374b565a438e2e5f4806694c
mileRANT <liem.htran@outlook.com>
2023-05-01 14:33:15 -0400

    Further practice with CSS, Javascript, DOM, HTML

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.querySelector("h1").innerHTML = "Good Bye";
 //document.getElementsByTagName("li")       //gives an array
 document.getElementsByTagName("li")[2].style.color = "purple";
 
-// document.getElementsByClassName("btn")[0].style.color = "green";
+document.getElementsByClassName("btn")[0].style.backgroundColor = "yellow";
 
 document.getElementsByTagName("input")[0].click();
 
@@ -14,3 +14,16 @@ document.getElementById("title").innerHTML = "Hello Again!";
 document.querySelector("#title").innerHTML = "And then bye again.";
 
 document.querySelector("li a").innerHTML = "Google google google";
+
+document.querySelector("button").classList.add("invisible"); //this adds class to it. proper way for manipulating styles. let css handle it.
+// document.querySelector("button").classList.remove("invisible");
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").classList.add("huge");
+
+document.querySelector("h1").innerHTML = "<em>Bah</em>";
+document.querySelector("h1").textContent = "Books";
+
+// attribute manipulation
+// document.querySelector("a").getAttribute("href")
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
